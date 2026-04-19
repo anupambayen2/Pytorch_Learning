ac3c4ccf59f8dd28bfbc59b0350fd81aecbcebcc
anupambayen2 <anupam.bayen2@gmail.com>
2026-04-19 21:52:17 +0530

    Files added

---
+++ Master_Pytorch/Jennis_Seemann_Course/Day-5/04_embeddings.py
@@ -0,0 +1,34 @@
+from transformers import BartTokenizer, BartModel
+import torch 
+from torch import nn
+
+custom_messages =[
+    'we have reales a new product, do you want to buy it?',
+    'Winner! Great Deal, call us to the product offer free',
+    'Tomorrow is my birthday, do you come to the party?',
+]
+
+tokenizer = BartTokenizer.from_pretrained("facebook/bart-base")
+
+out = tokenizer(
+        custom_messages,
+        padding=True,
+        max_length=512,
+        truncation=True,
+        return_tensors="pt"
+    )
+
+bart_model = BartModel.from_pretrained("facebook/bart-base")
+
+with torch.no_grad():
+    bart_model.eval()
+    # print(out)
+
+    pred = bart_model(
+        input_ids = out["input_ids"],
+        attention_mask = out["attention_mask"]
+    )
+    embeddings = pred.last_hidden_state.mean(dim=1)
+    print(embeddings.shape)
+    print(embeddings[0,:1])
+
--- Master_Pytorch/Jennis_Seemann_Course/Day-5/06_final_test_file.py
+++ Master_Pytorch/Jennis_Seemann_Course/Day-5/06_final_test_file.py
@@ -1,9 +1,39 @@
+from transformers import BartTokenizer, BartModel
 import sys
 import torch 
 from torch import nn
+from tqdm import tqdm
 
 import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
+
+
+tokenizer = BartTokenizer.from_pretrained("facebook/bart-base")
+bart_model = BartModel.from_pretrained("facebook/bart-base")
+
+def convertin_to_embeddings(messages):
+    embedding_list = []
+    for message in tqdm(messages):
+
+        out = tokenizer(
+            [message],
+            padding=True,
+            max_length=512,
+            truncation=True,
+            return_tensors="pt"
+        )
+
+        with torch.no_grad():
+            bart_model.eval()
+
+            pred = bart_model(
+                input_ids = out["input_ids"],
+                attention_mask = out["attention_mask"]
+            )
+            embeddings = pred.last_hidden_state.mean(dim=1).reshape(-1)
+            embedding_list.append(embeddings)
+
+    return torch.stack(embedding_list)
 
 
 df = pd.read_csv('./data/SMSSpamCollection',sep='\t',names=['type','message'])
@@ -15,17 +45,17 @@ df_train = df.sample(frac=0.8,random_state=42)
 df_val = df.drop(index=df_train.index)
 
 
-cv = CountVectorizer(max_features=1000)
-message_train = cv.fit_transform(df_train['message'])
-message_val = cv.transform(df_val['message'])
 
-X_train = torch.tensor(message_train.todense(),dtype=torch.float32)
-y_train = torch.tensor(df_train['spam'].values, dtype=torch.float32).reshape(-1,1)
+X_train = convertin_to_embeddings(df_train['message'].tolist())
+X_val = convertin_to_embeddings(df_val['message'].tolist())
 
-X_val = torch.tensor(message_val.todense(),dtype=torch.float32)
+# X_train = torch.tensor(message_train.todense(),dtype=torch.float32)
+# y_train = torch.tensor(df_train['spam'].values, dtype=torch.float32).reshape(-1,1)
+
+y_train = torch.tensor(df_train['spam'].values,dtype=torch.float32).reshape(-1,1)
 y_val = torch.tensor(df_val['spam'].values, dtype=torch.float32).reshape(-1,1)
 
-model = nn.Linear(1000,1)
+model = nn.Linear(768,1)
 # loss_fn = torch.nn.MSELoss()
 loss_fn = torch.nn.BCEWithLogitsLoss()
 optimizer = torch.optim.SGD(model.parameters(), lr=0.02)
@@ -71,13 +101,11 @@ evaluate_model(X_val,y_val)
 print("")
 
 
-custom_messages = cv.transform([
+X_custom = convertin_to_embeddings([
     'we have reales a new product, do you want to buy it?',
     'Winner! Great Deal, call us to the product offer free',
     'Tomorrow is my birthday, do you come to the party?'
 ])
-
-X_custom = torch.tensor(custom_messages.todense(), dtype=torch.float32)
 
 model.eval()
 with torch.no_grad():
